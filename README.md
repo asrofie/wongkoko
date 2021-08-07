cf81a50a769ab57f5b4fc6e8bfea38bc979e9e45
Asrofie <asrofie@gmail.com>
2021-08-07 23:18:27 +0700

    refactor controller

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # wongkoko
 Another freelance project
 https://projects.co.id/public/browse_projects/view/0ac718/generate-api-dari-csv
+
+# Cara ubah lokasi folder.
+- 1) Buka project
+- 2) Edit file .env dengan notepad
+- 3) Cari FOLDER_FILE=E:\Projects\wongkoko2\import
+- 4) Ubah lokasi nya misal folder berada di D:\data_saya\dimana ya, maka edit baris tersebut menjadi:
+- FOLDER_FILE=D:\\data_saya\\dimana ya
+- Karakter slash ditulis 2 kali
+- 5) Simpan file
+
+## Terima Kasih
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use PHPHtmlParser\Dom;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,83 @@ use App\Http\Controllers\Controller;
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/margin',  [
-    'as' => 'get_margin', 'uses' => 'Controller@indexAction'
-]);
+$router->get('/api/data',  function(Request $request) {
+    $file = $request->get('file');
+    if (!$file) {
+        return response(['status'=>false], 404);
+    }
+
+    $filePath = getenv('FOLDER_FILE').DIRECTORY_SEPARATOR.$file;
+    if (!file_exists($filePath)) {
+        return response(['status'=>false], 404);
+    }
+    $extensions = explode('.', $file);
+    $array = [];
+    if (strtolower($extensions[count($extensions)-1]) == 'csv') {
+        $row = 1;
+        $colums = [];
+        if (($handle = fopen($filePath, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                if ($row > 1) {
+                    $values = [];
+                    foreach($colums as $i => $col) {
+                        $val = null;
+                        if (isset($data[$i])) {
+                            $val = $data[$i];
+                        }
+                        $values[$col] = $val;
+                    }
+                    $array[] = $values;
+                }
+                else {
+                    for ($c=0; $c < $num; $c++) {
+                        $colums[$c] = $data[$c];
+                    }
+                }
+                $row++;
+                
+            }
+            fclose($handle);
+        }
+    }
+    else {
+        $string = file_get_contents($filePath);
+        $string = preg_replace("/<!--(.|\s)*?-->/", "", $string);
+        $dom = new Dom;
+        $dom->loadStr($string);
+        $tables = $dom->find('table');
+        $table = $tables[0];
+        $rows = $table->find('tr');
+        $column = [];
+        $array = [];
+        foreach($rows as $index => $row) {
+            if ($index == 0) {
+                $cols = $row->find('th');
+                foreach($cols as $i => $col) {
+                    $column[$i]= $col->text;
+                }
+            }
+            else {
+                $cols = $row->find('td');
+                $value = [];
+                foreach($cols as $i => $col) {
+                    $val = $col->text;
+                    if ($val && $column[$i] == 'Date') {
+                        try {
+                            $date = Carbon::parse($val);
+                            $val = $date->format('Y-m-d H:i:s');
+                        }
+                        catch(\Exception $e) {
+                            $val = $e->getMessage();
+                        }
+                        
+                    }
+                    $value[$column[$i]] = $val;
+                }
+                $array[] = $value;
+            }
+        }
+    }
+    return response($array);
+});
